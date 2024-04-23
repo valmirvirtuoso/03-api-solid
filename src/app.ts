03b5a8825550ca2395f74a4a531efc591d93f154
Valmir Virtuoso <virtuoso751@gmail.com>
2024-04-23 02:39:04 -0300

    feat: create all tables in database and configuration of docker

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,13 @@
-import fastify from "fastify";
+import fastify from 'fastify'
+import { PrismaClient } from '@prisma/client'
 
 export const app = fastify()
+
+const prisma = new PrismaClient()
+
+prisma.user.create({
+  data: {
+    name: 'Valmir Mendes Virtuoso Filho',
+    email: 'virtuoso751@gmail.com',
+  },
+})
