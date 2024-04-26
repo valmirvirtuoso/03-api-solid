c8b9a15d6e071e666c6486a0fd83070689e2c0e7
Valmir Virtuoso <virtuoso751@gmail.com>
2024-04-26 20:34:43 -0300

    feat: create user

--- src/env/index.ts
+++ src/env/index.ts
@@ -1,20 +1,17 @@
 import 'dotenv/config'
-import { z} from "zod"
+import { z } from 'zod'
 // validar variaveis ambiente
 
 const envSchema = z.object({
-    NODE_ENV: z.enum(['dev', 'test', 'production']).default('dev'),
-    PORT: z.coerce.number().default(3333)
+  NODE_ENV: z.enum(['dev', 'test', 'production']).default('dev'),
+  PORT: z.coerce.number().default(3333),
 })
 
 const _env = envSchema.safeParse(process.env)
 
 if (_env.success === false) {
-    console.error('❌ Invalid environment variables', _env.error.format())
-    throw new Error('❌ Invalid environment variables.')
+  console.error('❌ Invalid environment variables', _env.error.format())
+  throw new Error('❌ Invalid environment variables.')
 }
 
 export const env = _env.data
-
-
-
